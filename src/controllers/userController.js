5b798375366a91e66fea7f506b77247afde98ba6
ajayahir3398 <bandhiya.ajay@gmail.com>
2025-02-12 23:17:19 +0530

    changes related to register and login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,44 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
+require('dotenv').config();
 
-// @desc Get all users
+const JWT_SECRET = process.env.JWT_SECRET;
+
 exports.login = async (req, res) => {
   try {
-    const users = await User.find();
-    res.json(users);
+    const { email, password } = req.body;
+
+    // Validate input
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // Validate password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign(
+      { userId: user._id, email: user.email, sessionId: new Date().getTime() },
+      JWT_SECRET,
+      { expiresIn: "1d" } // Token expires in 1 day
+    );
+
+    return res.status(200).json({ message: "Login successful", token, user });
   } catch (error) {
-    res.status(500).json({ message: "Server Error" });
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
   }
 };
 
@@ -30,6 +61,7 @@ exports.register = async (req, res) => {
       password,
       confirmPassword,
     } = req.body;
+    console.log(req.body);
 
     // Check if passwords match
     if (password !== confirmPassword) {
