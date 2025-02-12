5f1d271ffd480ad8dbebea236148b61a69d607c6
ajayahir3398 <bandhiya.ajay@gmail.com>
2025-02-12 23:38:56 +0530

    added swagger doc

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const connectDB = require("./src/config/db");
 const routes = require("./src/routes/authRoute");
+const swaggerSetup = require("./src/config/swagger");
 
 dotenv.config();
 connectDB();
@@ -15,6 +16,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/auth", routes);
+
+// Setup Swagger
+swaggerSetup(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -3,7 +3,84 @@ const { login, register } = require("../controllers/userController");
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Login a user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *       400:
+ *         description: Invalid input
+ *       401:
+ *         description: Invalid email or password
+ */
 router.get("/login", login);
+
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               businessName:
+ *                 type: string
+ *               name:
+ *                 type: string
+ *               gstNo:
+ *                 type: string
+ *               mobileNo:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               gender:
+ *                 type: string
+ *               dob:
+ *                 type: string
+ *               street:
+ *                 type: string
+ *               street2:
+ *                 type: string
+ *               city:
+ *                 type: string
+ *               state:
+ *                 type: string
+ *               pinCode:
+ *                 type: string
+ *               country:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               confirmPassword:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Invalid input
+ *       500:
+ *         description: Server error
+ */
 router.post("/register", register);
 
 module.exports = router;
